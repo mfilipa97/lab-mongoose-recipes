e30abc7239b25d2ce1eb1ab0bd1bf1f3d75f04c8
John Doe <marrian1997@hotmail.com>
2023-08-04 08:51:10 +0100

    solved

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 //Method 1 : Using Async Await
+
+let Ramen = {
+  title:"Ramen",
+  level: "UltraPro Chef",
+  ingredients:["Noodles","Chicken","BokChoy","Naruto","Miso Paste", "Soy Sauce", "Furikake", "Eggs"],
+  cuisine:"Asian",
+  dishType:"main_course",
+  duration: 10,
+}
 
 const manageRecipes = async () => {
   try {
@@ -19,9 +28,30 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
-  } catch (error) {
-    console.log(error);
-  }
+  await Recipe.create(Ramen);
+  console.log(Ramen)
+
+  /*let recipes = await Recipe.insertMany(data);
+  for(let i = 0; i<recipes.length; i++){
+    console.log(recipes[i].title)
+  }*/
+  let recipes = await Recipe.insertMany(data);
+recipes.forEach(recipe => {
+  console.log(recipe.title);
+});
+  await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100});
+  console.log("Updated Rigatoni alla Genovese duration period to 100")
+
+  await Recipe.deleteOne({title: "Carrot Cake"});
+  console.log("Removed Carrot Cake from recipes!");
+
+ 
+  mongoose.disconnect();
+  /*mongoose.connection.close(function() {console.log('Mongoose connection closed')});*/
+
+} catch (error) {
+  console.log(error);
+}
 };
 
 manageRecipes();
